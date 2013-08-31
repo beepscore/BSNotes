0fef3fee02eb3933af27e490fe479230329e7323
beepscore <support@beepscore.com>
2013-08-31 16:50:38 -0700

    In Intellij, create new project BSNotes with Android application module.

---
+++ src/com/beepscore/BSNotes/NotesActivity.java
@@ -0,0 +1,15 @@
+package com.beepscore.BSNotes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NotesActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
