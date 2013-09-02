fd7ef3d9155b38797ae4f1b85f84792daec7e2fd
beepscore <support@beepscore.com>
2013-09-02 00:39:34 -0700

    In Finder add directory data and file NoteItem.java.
    Finder shows path as src/beepscore/bsnotes/data, SourceTree shows path as src/beepscore/BSNotes/data. Is this a SourceTree bug?
    Open Intellij, it sees new file. Project runs in emulator, but I don't think it's using NoteItem.java yet.

---
+++ src/com/beepscore/BSNotes/data/NoteItem.java
@@ -0,0 +1,50 @@
+package com.beepscore.bsnotes.data;
+
+import android.annotation.SuppressLint;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class NoteItem {
+
+    private String key;
+    private String text;
+
+    // accessor methods
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    // suppress warnings about SimpleDateFormat class
+    @SuppressLint("SimpleDateFormat")
+    // returns a NoteItem instance
+    // static method can be called on a class
+    public static NoteItem getNew() {
+
+        // Use en_US locale format. This enables alpha sort to be chronological.
+        Locale locale = new Locale("en_US");
+        Locale.setDefault(locale);
+
+        String pattern = "yyyy-MM-dd HH:mm:ss Z";
+        SimpleDateFormat formatter = new SimpleDateFormat(pattern);
+        String key = formatter.format(new Date());
+
+        NoteItem note = new NoteItem();
+        note.setKey(key);
+        note.setText("");
+        return note;
+    }
+
+}
