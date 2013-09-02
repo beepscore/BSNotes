f1c17187ea26c93e1f8353bbf7d01bac64cdb84d
beepscore <support@beepscore.com>
2013-09-02 12:23:19 -0700

    In NotesActivity add private variable datasource, in onCreate instantiate datasource, get a note from it, and log the key.
    LogCat shows key dateTime stamp.

--- src/com/beepscore/BSNotes/NotesActivity.java
+++ src/com/beepscore/BSNotes/NotesActivity.java
@@ -2,14 +2,28 @@ package com.beepscore.bsnotes;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import com.beepscore.bsnotes.data.NoteItem;
+import com.beepscore.bsnotes.data.NotesDataSource;
+
+import java.util.List;
 
 public class NotesActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+
+    private NotesDataSource datasource;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        datasource = new NotesDataSource();
+        List<NoteItem> notes = datasource.findAll();
+        NoteItem note = notes.get(0);
+
+        Log.i("NOTES", note.getKey());
     }
 }
