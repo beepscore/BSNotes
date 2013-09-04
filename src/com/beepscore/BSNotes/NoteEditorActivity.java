b2c1654329e1a797437d0f19023f8d4633ee7473
beepscore <support@beepscore.com>
2013-09-04 12:10:06 -0700

    In NoteEditorActivity.java onCreate() use the intent to recreate the note.
    Now from initial screen, when user taps "+" icon the app navigates to note editor activity.
    When user taps back the app navigates back, but it doesn't save note edits.

--- src/com/beepscore/BSNotes/NoteEditorActivity.java
+++ src/com/beepscore/BSNotes/NoteEditorActivity.java
@@ -1,17 +1,35 @@
 package com.beepscore.bsnotes;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import com.beepscore.bsnotes.data.NoteItem;
 
 /**
  * Created by stevebaker on 9/3/13.
  */
 public class NoteEditorActivity extends Activity {
 
+    private NoteItem note;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_editor);
+
+        Intent intent = this.getIntent();
+
+        // author calls intent to recreate the note,
+        // probably would be clearer to pass the note
+        note = new NoteItem();
+        note.setKey(intent.getStringExtra("key"));
+        note.setText(intent.getStringExtra("text"));
+
+        EditText et = (EditText) findViewById(R.id.noteText);
+        et.setText(note.getText());
+        // position cursor at end of text
+        et.setSelection(note.getText().length());
     }
 
 }
