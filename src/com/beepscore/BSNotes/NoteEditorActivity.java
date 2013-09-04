c6a06ef2d7a96fb22dfa68595c6df002944605c7
beepscore <support@beepscore.com>
2013-09-03 22:25:48 -0700

    Add activity NoteEditorActivity.java. In AndroidManifest.xml add the new activity.

---
+++ src/com/beepscore/BSNotes/NoteEditorActivity.java
@@ -0,0 +1,17 @@
+package com.beepscore.bsnotes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by stevebaker on 9/3/13.
+ */
+public class NoteEditorActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_editor);
+    }
+
+}
